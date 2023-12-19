b0e208aad9063fea571694c1227dab022e84e839
aondrej ryan <aondrejryan11@gmail.com>
2023-12-18 20:54:52 -0800

    ddd

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -11,6 +11,7 @@ function Home() {
         e.preventDefault()
         console.log(level)
     }
+    console.log('lol')
   return (
     <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
   <div className="container max-w-screen-lg mx-auto">
